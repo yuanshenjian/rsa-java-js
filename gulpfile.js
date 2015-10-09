5e7dbb934968448b0efb7a58b1b452183cd8e891
Shenjian YUAN <sjyuan@thoughtworks.com>
2015-10-09 15:01:35 +0800

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var paths = {
+    scripts: ['js/index.js','js/main.js']
+};
+gulp.task('clean',function(){
 
-gulp.task('default', function() {
-    // place code for your default task here
+});
+
+gulp.task('default', function () {
+    gulp.src(paths.scripts).pipe(uglify()).pipe(concat('all.min.js')).pipe(gulp.dest('dest'));
 });
