c216f1cb5736c06f1ed34e214c5d2d7491317eda
Shenjian YUAN <sjyuan@thoughtworks.com>
2015-10-09 15:15:33 +0800

    use gulp to build js

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var paths = {
-    scripts: ['js/index.js','js/main.js']
+    scripts: [
+        'node_modules/jquery/dist/jquery.min.js'
+        , 'node_modules/jsencrypt/bin/jsencrypt.js'
+        , 'src/main/resources/static/js/encryption.js'
+    ]
 };
-gulp.task('clean',function(){
+gulp.task('clean', function () {
 
 });
 
 gulp.task('default', function () {
-    gulp.src(paths.scripts).pipe(uglify()).pipe(concat('all.min.js')).pipe(gulp.dest('dest'));
+    gulp.src(paths.scripts).pipe(uglify()).pipe(concat('main.min.js')).pipe(gulp.dest('src/main/resources/static/js'));
 });
